67df96e5958dc71e2bde94a13be92889594940e3
RAHAMATULLA <54510712+mrfirdous192@users.noreply.github.com>
2019-11-08 01:24:48 +0530

    Add files via upload

---
+++ ocr_core.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+def ocr_core(filename):
+    #This function will handle the core OCR processing of images.
+    text = pytesseract.image_to_string(Image.open(filename))# We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+#print(ocr_core(file))
